cd7ae091110e4fb72e22cad919268edbf7e51d20
TakeoYakura <92492416+TakeoYakura@users.noreply.github.com>
2022-04-12 16:37:16 -0700

    added todos from axios

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,9 +1,51 @@
 import React from 'react'
+import axios from 'axios'
+import Form from './Form'
+import TodoList from './TodoList'
 
 const URL = 'http://localhost:9000/api/todos'
 
 export default class App extends React.Component {
+  state = {
+    todos: [],
+    error: '',
+    todoNameInput: '',
+    displayCompleted: true,
+  }
+
+  fetchTodos = () => {
+    axios.get(URL)
+      .then(res => {
+        this.setState({...this.state, todos: res.data.data})
+      })
+      .catch(err => {
+         this.setState({...this.state, error: err.response.data.message})
+      })
+  }
+
+  componentDidMount() {
+    this.fetchTodos()
+  }
+
   render() {
-    return null
+    return (
+      <div>
+        <h1>Todo List</h1>
+
+        <h2>Todos:</h2>
+        <ul>
+          {this.state.todos.map(todo => {
+            return <li>{todo}</li>
+          })}
+        </ul>
+        
+        <form>
+          <input />
+          <button>Submit</button>
+        </form>
+        
+        <button>Hide Completed</button>
+      </div>
+    )
   }
 }
